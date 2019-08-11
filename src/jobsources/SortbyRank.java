91c4f5438e82f82d7156bd142603db0559481f02
Antonio Rodriguez <portexploits@gmail.com>
2019-08-11 11:47:58 -0700

    Fixed the Read/Write Objects to file

--- src/jobsources/SortbyRank.java
+++ src/jobsources/SortbyRank.java
@@ -2,9 +2,10 @@ package jobsources;
 
 import jobsources.files_that_work_with_job_data.JobData;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class SortbyRank implements Comparator<JobData> {
+public class SortbyRank implements Comparator<JobData>, Serializable {
     @Override
     public int compare(JobData a, JobData b) {
         return b.getRank() - a.getRank();
